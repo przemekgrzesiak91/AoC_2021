22d08e7687e025328b06ed997f6ee6559fd98c4e
Przemyslaw Grzesiak <przemek.grzesiak91@gmail.com>
2021-12-11 05:54:14 +0100

    Added Day 10.2

--- day10.py
+++ day10.py
@@ -3,11 +3,16 @@ with open('data\day10.txt') as f:
     data = f.read().split('\n')
 
 close_chunks = [')',']','}','>']
-pairs = {')' : ['(',3],
-         ']' : ['[',57],
-         '}' : ['{',1197],
-         '>' : ['<',25137]}
+pairs = {')' : ['(',3,1],
+         ']' : ['[',57,2],
+         '}' : ['{',1197,3],
+         '>' : ['<',25137,4]}
+pairs2 = {'(' : 1,
+         '[' : 2,
+         '{' : 3,
+         '<' : 4}
 
+incomplete = []
 sum = 0
 for row in data:
     row = list(row)
@@ -27,8 +32,23 @@ for row in data:
                     sum += pairs[row[i]][1]
                     cond = False
                     break
-
+            if i == len(row) - 1:
+                incomplete.append(row)
 print(sum)
+
+
+sum2 = []
+for row in incomplete:
+    #print(row[::-1])
+    sum = 0
+    for chunk in row[::-1]:
+        sum *= 5
+        sum += pairs2[chunk]
+    sum2.append(sum)
+
+middle = int(len(sum2)/2)
+sum2 = sorted(sum2)
+print(sum2[middle])
 
 
 
