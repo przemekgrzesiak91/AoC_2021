80dff2e69d276dc2461d0d37cf0289b018d2449c
Przemyslaw Grzesiak <przemek.grzesiak91@gmail.com>
2021-12-22 05:18:51 +0100

    updated day 13

--- day13.py
+++ day13.py
@@ -8,23 +8,24 @@ with open('data\day13.txt') as f:
         elif x: pos.append(line.strip().split(','))
         elif not x: instructions.append(line.strip())
 
-print(pos)
-print(instructions)
-
 max_x = max([int(x[0]) for x in pos ]) +1
-max_y = max([int(y[1]) for y in pos ]) +1
+# change this !
+max_y = max([int(y[1]) for y in pos ]) +6
+# !!
 print(max_x, max_y)
-
 
 paper = [['.'] * max_x for _ in range(max_y)]
 for row in pos:
     x,y = int(row[0]), int(row[1])
     paper[y][x] = '#'
 
+
+
 def print_paper(paper):
     for line in paper:
-        #line = ''.join(line)
+        line = ''.join(line)
         print(line)
+
 def count(paper):
     sum = 0
     for row in paper:
@@ -34,31 +35,32 @@ def count(paper):
 
 #folds
 for fold in instructions:
-    print_paper(paper)
+    #print_paper(paper)
     how,value = fold.split(' ')[2].split('=')
     value = int(value)
+    print('-----')
     print(how,value)
+    print('max',max_x,max_y)
     if how == 'x':
-        new_paper = [['.'] * value for _ in range(max_y)]
-        for j in range(0,max_y):
-            for i in range(0,value):
-                if paper[j][i] =='#' or paper[j][max_x-1-i] == '#':
+        new_paper = [['.'] * (value) for _ in range(max_y)]
+        for j in range(max_y-1,-1,-1):
+            for i in range(value-1,-1,-1):
+                #print(i, max_x-1-i,j)
+                if paper[j][i] == '#' or paper[j][max_x-1-i] == '#' and i != max_x-1-i:
                     new_paper[j][i] = '#'
                 else:
-                    new_paper[j][i] = '.'
+                    new_paper[j][i] = ' '
         paper = new_paper
         max_x = value
-
-
-    if how == 'y':
+    elif how == 'y':
         new_paper = [['.'] * max_x for _ in range(value)]
-        for j in range(0, value):
-            for i in range(0, max_x):
-
-                if paper[j][i] == '#' or paper[max_y-1-j][i] == '#':
+        for j in range(value-1,-1,-1):
+            for i in range(max_x-1,-1,-1):
+                #print(j, max_y-1-j, i)
+                if paper[j][i] == '#' or paper[max_y-1-j][i] == '#' and j != max_y-1-j:
                     new_paper[j][i] = '#'
                 else:
-                    new_paper[j][i] = '.'
+                    new_paper[j][i] = ' '
         paper = new_paper
         max_y = value
     count(paper)
